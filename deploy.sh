725417b9000a04c7b421977352f2f530db50a783
chiragshrimal <chirag1711shrimal@gmail.com>
2025-06-11 10:29:01 +0530

    fix error

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,5 @@
-#!/bin/bash
-
-# Exit on any error
-set -e
-
-# Load NVM (needed because GitHub Actions uses non-interactive SSH)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Go to the project directory
-cd /home/ubuntu/Basic-project-on-next.js
-
-# Pull latest changes from main branch
+cd Basic-project-on-next.js
 git pull origin main
-
-# Install dependencies
 npm install
-
-# Build the project
 npm run build
-
-# Start the application
 npm run start
